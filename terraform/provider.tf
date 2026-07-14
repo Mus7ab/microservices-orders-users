a5f1d62833dd054b2791feaeedded193a77e1f70
Musaab Mohamedani <musaab.cloud@gmail.com>
2026-07-14 08:39:24 +0000

    fix: migrate to S3 remote state backend proactively, before any CI/CD exists

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+backend "s3" {
+    bucket       = "microservices-orders-users-tfstate-342677169816"
+    key          = "microservices-orders-users/terraform.tfstate"
+    region       = "ap-south-2"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
 provider "aws" {
